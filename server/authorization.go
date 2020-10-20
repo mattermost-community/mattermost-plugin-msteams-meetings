c99190204ce00cec302bdedb6adad9786b7dac75
Daniel Espino García <larkox@gmail.com>
2020-10-20 18:27:38 +0200

    Add attendees to meeting (#19)
    * Add users as attendees
    
    * Refactoring and adding UPN to request
    
    * Fix lint
    
    * Use list instead of variadic

---
+++ server/authorization.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/mattermost/mattermost-plugin-msteams-meetings/server/remote"
+	msgraph "github.com/yaegashi/msgraph.go/beta"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/microsoft"
+)
+
+type authError struct {
+	Message string `json:"message"`
+	Err     error  `json:"err"`
+}
+
+func (ae *authError) Error() string {
+	errorString, _ := json.Marshal(ae)
+	return string(errorString)
+}
+
+var oAuthMessage string = "[Click here to link your Microsoft account.](%s/plugins/" + manifest.Id + "/oauth2/connect?channelID=%s)"
+
+func (p *Plugin) authenticateAndFetchUser(userID, userEmail, channelID string) (*msgraph.User, *authError) {
+	var user *msgraph.User
+	var err error
+
+	siteURL, err := p.getSiteURL()
+	if err != nil {
+		p.API.LogError("authenticateAndFetchUser, cannot get site URL", "error", err.Error())
+		return nil, &authError{Message: "Cannot get Site URL. Contact your sys admin.", Err: err}
+	}
+
+	userInfo, apiErr := p.store.GetUserInfo(userID)
+	oauthMsg := fmt.Sprintf(
+		oAuthMessage,
+		siteURL, channelID)
+
+	if apiErr != nil || userInfo == nil {
+		return nil, &authError{Message: oauthMsg, Err: apiErr}
+	}
+	user, err = p.getUserWithToken(userInfo.OAuthToken)
+	if err != nil || user == nil {
+		return nil, &authError{Message: oauthMsg, Err: apiErr}
+	}
+
+	return user, nil
+}
+
+func (p *Plugin) disconnect(userID string) error {
+	return p.store.RemoveUser(userID)
+}
+
+func (p *Plugin) getOAuthConfig() (*oauth2.Config, error) {
+	config := p.getConfiguration()
+
+	clientID := config.OAuth2ClientID
+	clientSecret := config.OAuth2ClientSecret
+	clientAuthority := config.OAuth2Authority
+
+	siteURL, err := p.getSiteURL()
+	if err != nil {
+		return nil, err
+	}
+
+	redirectURL := fmt.Sprintf("%s/plugins/%s/oauth2/complete", siteURL, manifest.Id)
+
+	return &oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectURL,
+		Scopes: []string{
+			"offline_access",
+			"OnlineMeetings.ReadWrite",
+		},
+		Endpoint: microsoft.AzureADEndpoint(clientAuthority),
+	}, nil
+}
+
+func (p *Plugin) getUserWithToken(token *oauth2.Token) (*msgraph.User, error) {
+	conf, err := p.getOAuthConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	client := remote.NewClient(conf, token, p.API)
+
+	user, err := client.GetMe()
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
--- server/remote/meeting.go
+++ server/remote/meeting.go
@@ -8,23 +8,47 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/mattermost/mattermost-plugin-msteams-meetings/server/store"
 	"github.com/pkg/errors"
 	msgraph "github.com/yaegashi/msgraph.go/beta"
 )
 
-func (c *Client) CreateMeeting(userID string) (*msgraph.OnlineMeeting, error) {
+func (c *Client) CreateMeeting(creator *store.UserInfo, attendeesIDs []*store.UserInfo) (*msgraph.OnlineMeeting, error) {
 	ctx := context.Background()
 	start := time.Now()
 	end := start.Add(1 * time.Hour)
 	subject := "Mattermost Meeting"
+	attendees := []msgraph.MeetingParticipantInfo{}
+	for _, attendee := range attendeesIDs {
+		attendees = append(attendees, msgraph.MeetingParticipantInfo{
+			Identity: &msgraph.IdentitySet{
+				User: &msgraph.Identity{
+					ID: &attendee.RemoteID,
+				},
+			},
+			Upn: &attendee.UPN,
+		})
+	}
+
 	in := msgraph.OnlineMeeting{
 		StartDateTime: &start,
 		EndDateTime:   &end,
 		Subject:       &subject,
+		Participants: &msgraph.MeetingParticipants{
+			Organizer: &msgraph.MeetingParticipantInfo{
+				Identity: &msgraph.IdentitySet{
+					User: &msgraph.Identity{
+						ID: &creator.RemoteID,
+					},
+				},
+				Upn: &creator.UPN,
+			},
+			Attendees: attendees,
+		},
 	}
 	out := msgraph.OnlineMeeting{}
 
-	err := c.builder.Users().ID(userID).OnlineMeetings().Request().JSONRequest(ctx, http.MethodPost, "", &in, &out)
+	err := c.builder.Users().ID(creator.RemoteID).OnlineMeetings().Request().JSONRequest(ctx, http.MethodPost, "", &in, &out)
 	if err != nil {
 		return nil, errors.Wrap(err, "cannot create meeting")
 	}
